b6190677fad306f08afb78d0f492f0a9649244a5
kimiscool0011 <xoxiday01@gmail.com>
2026-04-12 21:36:08 +0530

    pw removed

--- api/responses.js
+++ api/responses.js
@@ -1,16 +1,8 @@
-import { kv } from '@vercel/kv';
-
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
-  const pass = req.headers['x-admin-password'];
-
-  if (pass !== ADMIN_PASSWORD) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  const data = await kv.lrange('responses', 0, -1);
-  const parsed = data.map(item => JSON.parse(item));
+  const data = await kv.lrange("responses", 0, -1);
+  const parsed = data.map((item) => JSON.parse(item));
 
   res.json(parsed);
 }
