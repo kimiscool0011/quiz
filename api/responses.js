6866cb231318cadfc52f9bb1a8e724063897a2b1
kimiscool0011 <xoxiday01@gmail.com>
2026-04-13 07:27:31 +0530

    fixed crash

--- api/responses.js
+++ api/responses.js
@@ -1,8 +1,19 @@
 import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
-  const data = await kv.lrange("responses", 0, -1);
-  const parsed = data.map((item) => JSON.parse(item));
+  try {
+    const data = await kv.lrange("responses", 0, -1);
 
-  res.json(parsed);
+    const parsed = data.map((item) => {
+      try {
+        return JSON.parse(item);
+      } catch (e) {
+        return { error: "Bad entry", raw: item };
+      }
+    });
+
+    res.json(parsed);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
